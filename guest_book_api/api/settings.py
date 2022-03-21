0c28ea80773c63b4649ebb1c3414cb947a08645b
Henrique Dalla Corte <riquejdc@gmail.com>
2022-03-20 22:30:04 -0300

    Update settings.py

--- guest_book_api/api/settings.py
+++ guest_book_api/api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j&=wud2g!g0dzd=7r@z@ob-mb8rn2^y8@h55-r*-jr=d$@88ev'
+# SECRET_KEY = 'your-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
